37debbab68541e2bd987a2e47c9d3548e2f46ae0
Ansh Sharma <sharmaji.ansh05032005@gmail.com>
2026-06-08 06:49:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (16.45%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,22 +2,28 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
+        vector<int> ans(n , 1);
+       
+
         vector<int> pre(n) , suff(n);
+        pre[0] = nums[0];
+        suff[n - 1] = nums[n - 1];
 
-        pre[0] = 1; suff[n-1] = 1;
-        for(int i = 1; i <n ; i++){
-            pre[i] = pre[i-1]*nums[i-1];
+        for(int i = 1 ; i < n ; i++){
+            pre[i] = nums[i] * pre[i - 1];
         }
-        for(int i = n-2 ; i >= 0; i--){
-            suff[i] =suff[i + 1] * nums[i + 1];
-        }
-
-        vector<int> res(n);
-        for(int i = 0  ;i < n ; i++){
-            res[i] = pre[i]*suff[i];
-
+        for(int i = n - 2 ; i >= 0 ; i--){
+            suff[i] = nums[i] * suff[i + 1];
         }
 
-        return res;
+        for(int i = 0 ; i < n ; i++){
+
+            int left = (i == 0) ? 1 : pre[i - 1];
+            int right = (i == n - 1) ? 1 : suff[i + 1];
+
+            ans[i] = left * right;
+        }
+
+        return ans;
     }
 };
