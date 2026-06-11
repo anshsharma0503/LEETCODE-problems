fa2bd161dd7c36ef338337ef2dc912977441fcd5
Ansh Sharma <sharmaji.ansh05032005@gmail.com>
2026-06-11 15:23:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (75.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
-        unordered_map<char , int> mp;
 
-        for(auto ch : s){
-            mp[ch]++;
+        vector<int> freq(26 , 0);
+
+        for(int ch : s){
+            freq[ch - 'a']++;
         }
 
-        for(auto ch : t){
-            if(mp.find(ch) != mp.end()){
-                mp[ch]--;
-                if(mp[ch] == 0) mp.erase(ch);  
-            }
+        for(int ch : t){
+            freq[ch - 'a']--;
         }
 
-        if(mp.size() == 0) return true;
-        return false;
+        for(int i = 0 ; i < 26 ; i++){
+            if(freq[i] != 0) return false;
+        }
+
+        return true;
     }
 };
