ed1bc9678f6d80e3e9fe27a93f59327b3ddc5558
Ansh Sharma <sharmaji.ansh05032005@gmail.com>
2026-06-27 11:46:56 +0530

    Time: 41 ms (63.45%) | Memory: 45.5 MB (40.46%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,23 +2,19 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        
-        vector<int> pre(n);
-        pre[0] = nums[0];
-        for(int i = 1 ; i < n;  i++){
-            pre[i] = pre[i -1] + nums[i];
-        }
+        int currSum = 0;
+        int res =0 ;
 
-        unordered_map<int , int> mp;
-        mp[0]++;
+        unordered_map<int , int> freq;
+        freq[0]++;
 
-        int res = 0;
+        for(int i =0; i < n ; i++){
+            currSum += nums[i];
 
-        for(int i = 0 ;i < n; i++){
-            int need = pre[i] - k;
+            if(freq.find(currSum - k) != freq.end()) 
+                res += freq[currSum - k];
 
-            if(mp.find(need) != mp.end()) res += mp[need];
-            mp[pre[i]]++;
+            freq[currSum]++;
         }
 
         return res;
