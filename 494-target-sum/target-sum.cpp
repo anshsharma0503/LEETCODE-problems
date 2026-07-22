2305474311508e8feb0e3913845bb105599126ce
Ansh Sharma <sharmaji.ansh05032005@gmail.com>
2026-07-22 17:11:36 +0530

    Time: 852 ms (10.97%) | Memory: 11.8 MB (79.21%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
 
-    int ans;
+    int f(int i , int sum , vector<int>& nums, int tar){
+        if(i == nums.size()) return sum == tar;
 
-    void f(int i , int s , int tar , vector<int>& nums){
-        if (s > tar) return;
-        if (i == nums.size()) {
-            if (s == tar) ans++;
-                return;
-        }
+        int pos =  f(i + 1 , sum+ nums[i] ,nums , tar);
+        int neg = f(i + 1 ,sum  -nums[i] ,  nums , tar);
 
-        f(i + 1 , s , tar , nums);
-        f(i + 1 , s + nums[i] , tar , nums);
+        return pos + neg;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
-        for(int x : nums) sum += x;
-
-        int tar = (sum + target) / 2;
-
-        if ((sum + target) % 2 != 0) return 0;
-        ans =0;
-
-        f(0 , 0 , tar ,nums);
-        return ans;
+        return f(0 ,0, nums , target);
     }
 };
