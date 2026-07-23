a8deb2a989dfde21bf7ba3f463baeb20167d636e
Ansh Sharma <sharmaji.ansh05032005@gmail.com>
2026-07-23 15:43:13 +0530

    Time: 48 ms (19.45%) | Memory: 36.8 MB (33.89%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -3,23 +3,27 @@ public:
 
     vector<vector<int>> dp;
 
-    int f(int i , int amount, vector<int>& coins) {
-        if(amount == 0) return 1;
+    int f(int i , int amount, vector<int>& coins){
+        if(i == coins.size())
+            return 0;
 
-        if(i >= coins.size()) return 0;
+        if( amount == 0) 
+            return 1;
 
-        if(dp[i][amount] != -1) return dp[i][amount];
+        if(dp[i][amount] != -1)
+            return dp[i][amount];
 
-        int nottake = f(i + 1 , amount , coins);
+        int notTake = f(i + 1 , amount , coins);
 
-        int take = 0;
-        if(amount - coins[i] >= 0) take = f(i , amount - coins[i] , coins);
+        int take =0;
+        if(amount - coins[i] >= 0)
+            take = f(i , amount - coins[i] , coins);
 
-        return dp[i][amount]= take + nottake;
+        return dp[i][amount] = take + notTake;
     }
 
     int change(int amount, vector<int>& coins) {
         dp.resize(coins.size() , vector<int> (amount + 1 , -1));
-        return f(0 , amount , coins);
+        return f(0 ,  amount , coins);
     }
 };
