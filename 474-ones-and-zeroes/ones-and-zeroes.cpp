ae3c7eeb0efc599a11c6277ccd882f9a8962d8d3
Ansh Sharma <sharmaji.ansh05032005@gmail.com>
2026-07-23 11:30:41 +0530

    Time: 170 ms (39.16%) | Memory: 106 MB (39.43%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
-    int maxi;
+
     vector<vector<vector<int>>> dp;
 
-    int f(int i  ,  vector<string>& strs, int m, int n){
-        if(i == strs.size()) return 0;
+    int f(int i, int m, int n, vector<string>& strs){
 
-        if(dp[i][m][n] != -1) return dp[i][m][n];
+        if(i == strs.size())
+            return 0;
 
-        int zerocnt = 0 , onecnt =0;
-        for(auto x : strs[i]){
-            if(x == '0') zerocnt++;
-            else onecnt++;
+        if(dp[i][m][n] != -1)
+            return dp[i][m][n];
+
+        int zero = 0, one = 0;
+
+        for(char ch : strs[i]){
+            if(ch == '0') zero++;
+            else one++;
         }
+
+        int notTake = f(i + 1, m, n, strs);
+
         int take = 0;
 
-        if(zerocnt <= m && onecnt <= n)  take = 1 + f(i + 1  , strs , m- zerocnt , n - onecnt);
-        
-        int nottake = f(i + 1 , strs , m , n);
-        
-        return dp[i][m][n] =max(take, nottake);
+        if(zero <= m && one <= n)
+            take = 1 + f(i + 1, m - zero, n - one, strs);
 
+        return dp[i][m][n] = max(take, notTake);
     }
 
     int findMaxForm(vector<string>& strs, int m, int n) {
-        
-        dp.resize(strs.size() , vector<vector<int>> (m + 1, vector<int> (n + 1 , -1)));
-        return f(0 , strs , m , n);
+
+        int sz = strs.size();
+
+        dp.resize(sz,vector<vector<int>>(m + 1,vector<int>(n + 1, -1)));
+
+        return f(0, m, n, strs);
     }
 };
