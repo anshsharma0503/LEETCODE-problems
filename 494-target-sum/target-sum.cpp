cd637ef439d3336b3b33c6bff1140677ec007bc8
Ansh Sharma <sharmaji.ansh05032005@gmail.com>
2026-07-23 10:20:39 +0530

    Time: 2 ms (88.09%) | Memory: 15.8 MB (37.09%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,16 +1,39 @@
 class Solution {
 public:
 
-    int f(int i , int sum , vector<int>& nums, int tar){
-        if(i == nums.size()) return sum == tar;
-
-        int pos =  f(i + 1 , sum+ nums[i] ,nums , tar);
-        int neg = f(i + 1 ,sum  -nums[i] ,  nums , tar);
-
-        return pos + neg;
-    }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        return f(0 ,0, nums , target);
+
+        int total = accumulate(nums.begin(), nums.end(), 0);
+
+        if(abs(target) > total)
+            return 0;
+
+        if((target + total) % 2)
+            return 0;
+
+        int req = (target + total) / 2;
+
+        int n = nums.size();
+
+        vector<vector<int>> dp(n + 1,vector<int>(req + 1, 0));
+
+        dp[n][0] = 1;
+
+        for(int i = n - 1; i >= 0; i--) {
+
+            for(int sum = 0; sum <= req; sum++) {
+
+                int notTake = dp[i + 1][sum];
+
+                int take = 0;
+
+                if(nums[i] <= sum)
+                    take = dp[i + 1][sum - nums[i]];
+
+                dp[i][sum] = take + notTake;
+            }
+        }
+
+        return dp[0][req];
     }
 };
