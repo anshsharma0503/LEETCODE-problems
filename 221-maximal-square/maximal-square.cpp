36caca6d3061ba9e1d474f9ca46fbe79cb6ebcaf
Ansh Sharma <sharmaji.ansh05032005@gmail.com>
2026-08-19 04:21:35 +0530

    Time: 16 ms (27.58%) | Memory: 30.8 MB (85.48%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -6,26 +6,19 @@ public:
 
         vector<vector<int>> dp(m , vector<int> (n , 0));
 
-        for(int i = 0 ;i < m ; i++){
-            for(int j = 0; j < n ; j++){
-                if(matrix[i][j] == '0') dp[i][j] = 0;
-                else {
-                    dp[i][j] = 1;
-                    if(i > 0 && j > 0)
-                        dp[i][j] = 1 + min({dp[i-1][j], dp[i][j - 1], dp[i - 1][j -1]});
-                }
-            }
-        }
-
         int maxi=0;
+
         for(int i = 0 ;i < m ; i++){
-            for(int j = 0; j < n ; j++){
+            for(int j = 0; j < n ; j++){                    
+                    if( matrix[i][j] == '1'){
+                        dp[i][j] = 1;
+                        if(i > 0 && j > 0)
+                            dp[i][j] = 1 + min({dp[i-1][j], dp[i][j - 1], dp[i - 1][j -1]});
+                    }
+
                 maxi = max(maxi , dp[i][j]);
             }
         }
-
-        if(maxi == 1)
-            return 1;
 
         return maxi*maxi;
     }
