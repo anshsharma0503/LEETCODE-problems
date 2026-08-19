b921e5357ae2fe4fdd1230fdb90aebab23a70bf6
Ansh Sharma <sharmaji.ansh05032005@gmail.com>
2026-08-19 17:13:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (52.99%) - LeetSync

--- 174-dungeon-game/dungeon-game.cpp
+++ 174-dungeon-game/dungeon-game.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-
     vector<vector<int>> dp;
+    int f(int i , int j ,  vector<vector<int>>& grid){
+        int m = grid.size();
+        int n = grid[0].size();
 
-    int f(int i , int j ,vector<vector<int>>& d){
-        int m  =d.size();
-        int n = d[0].size();
-        if(i >= m || j >= n) return 1e9;
+        if(i == m || j == n)
+            return 1e9;
+    
+        if (i == m-1 && j == n-1)
+            return max(1, 1 - grid[i][j]);
 
-        if(dp[j][i] != -1) return dp[j][i];
- 
-        if(i == m - 1 && j == n - 1) return max(1 , 1- d[i][j]);
+        if(dp[i][j] != -1) return dp[i][j];
 
-        int req = min(f(i + 1 , j , d) , f(i , j + 1 , d));
+        int right = f(i , j + 1 ,  grid);
+        int down = f(i + 1 , j , grid);
 
-        return dp[j][i] = max(1 , req - d[i][j]);
+        int next = min(right , down);
+
+        return dp[i][j] = max(1 , next - grid[i][j]);
+
     }
 
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        int m  =dungeon.size();
+        int m = dungeon.size();
         int n = dungeon[0].size();
-        dp.resize(n , vector<int> (m , -1));
+
+        dp.assign(m, vector<int>(n, -1));
         return f(0 , 0 , dungeon);
     }
 };
