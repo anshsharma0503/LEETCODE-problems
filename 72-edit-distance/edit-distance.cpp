b97c5c6323875776d42413446d3dbccfbeccdab7
Ansh Sharma <sharmaji.ansh05032005@gmail.com>
2026-08-21 16:04:37 +0530

    Time: 15 ms (16.14%) | Memory: 14.4 MB (14.92%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -2,22 +2,31 @@ class Solution {
 public:
 
     vector<vector<int>> dp;
-    int f(int i , int j , string s1 , string s2){
-        if(i < 0) return j + 1;
-        if(j < 0) return i + 1;
+
+    int f(int i , int j , string &word1, string &word2){
+        int n = word1.length();
+        int m = word2.length(); 
+
+        if(i == n && j!= m) return m-j;
+        if(j == m && i != n) return n - i;
+
+        if(i == n && j == m) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(s1[i] == s2[j]) return f(i - 1 , j - 1 , s1 , s2);
+        if(word1[i] == word2[j]) return f(i + 1 , j + 1 , word1 , word2);
 
-        return dp[i][j] = min({ 1 + f(i , j - 1 , s1  ,s2) , 1 + f(i - 1 , j , s1 , s2) ,
-         1 + f(i - 1 , j - 1 , s1 , s2)});
+        int insert = f(i , j + 1 , word1 , word2);
+        int del = f(i+1 , j  , word1 , word2);
+        int replace = f(i + 1 , j + 1 , word1 , word2);
+
+        return dp[i][j] = 1 + min({insert , del , replace});
     }
 
     int minDistance(string word1, string word2) {
         int n = word1.length();
-        int m = word2.length();
-        dp.resize(n , vector<int> (m , -1));
-        return f(n - 1 , m - 1 ,word1 , word2);
+        int m = word2.length(); 
+        dp.assign(n , vector<int> (m ,-1));
+        return f(0 , 0 , word1 , word2);
     }
 };
